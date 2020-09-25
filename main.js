b80e75164b745663d619622662d1e9d28ac6435d
othmanekahtal <othmanekahtal@gmail.com>
2020-09-25 15:56:20 +0100

    change html and styling css

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 window.addEventListener("load", () => {
   //   document.getElementsByClassName("preloader")[0].style.display = "none";
   document.querySelector(".preloader").style.display = "none";
-  document.querySelector(".content").style.display = "block";
-});
-
-document.body.addEventListener("click", () => {
-  document.getElementById("demo").innerHTML = event.target.tagName;
-
-  //   or
-
-  document.getElementById("demo").innerHTML = event.target.nodeName;
+  document.querySelector(".img").classList.add("img__add");
 });
